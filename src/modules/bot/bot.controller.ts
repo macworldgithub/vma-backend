b320c582479230e82735d531d5b94e4680f22140
Hammad Lodhi <hammadak05@gmail.com>
2026-07-16 16:09:01 +0500

    recall webhook fix

--- src/modules/bot/bot.controller.ts
+++ src/modules/bot/bot.controller.ts
@@ -32,18 +32,21 @@ export class BotController {
     }
 
     switch (payload.event) {
-      case 'bot.status_change':
-        this.logger.log(`Bot ${botId} status changed to ${data.status.code}`);
-        await this.meetingModel.findByIdAndUpdate(meeting._id, { botStatus: data.status.code });
+      case 'bot.joining_call':
+      case 'bot.in_waiting_room':
+      case 'bot.in_call_recording':
+      case 'bot.in_call_not_recording':
+        this.logger.log(`Bot ${botId} status changed to ${payload.event}`);
+        await this.meetingModel.findByIdAndUpdate(meeting._id, { botStatus: payload.event });
         break;
       
-      case 'bot.meeting_ended':
+      case 'bot.call_ended':
         this.logger.log(`Meeting ended for Bot ${botId}`);
         await this.meetingModel.findByIdAndUpdate(meeting._id, { status: 'ENDED' });
         break;
 
-      case 'bot.transcript_ready':
-        this.logger.log(`Transcript ready for Bot ${botId}`);
+      case 'bot.done':
+        this.logger.log(`Processing complete for Bot ${botId}, fetching transcript...`);
         // Trigger summarization logic asynchronously
         this.botService.processTranscript(botId, meeting).catch((err) => {
           this.logger.error(`Error processing transcript: ${err.message}`);
